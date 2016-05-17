0016c0ba0cf5641685a44fcba2afd72ecb40c191
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-05-16 20:40:16 -0500

    test

--- contactos/models.py
+++ contactos/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 from django.db import models
 from django.core.exceptions import ValidationError
-
+# 95172a7324019870220fd4304684e07d
 def validate_length(value,length=24):
     print len(str(value))
     if len(str(value)) != length:
@@ -23,13 +23,13 @@ class address(models.Model):
     
     def __unicode__(self):
         return self.street + ' ' + self.zip_code
-class smes(models.Model):
+class smes(User):
     RFC = models.CharField(max_length=13, choices=gen,verbose_name=u'RFC: ')
     ciec = models.CharField(max_length=30, verbose_name=u'CIEC:')
     
     def __unicode__(self):
         return self.RFC 
-class investor(models.Model):
+class investor(User):
     name = models.CharField(max_length=30, verbose_name=u'Name')
     last_name = models.CharField(max_length=30, verbose_name=u'Last name')
     gender = models.CharField(max_length=1, choices=gen,verbose_name=u'Gender: ')
@@ -49,7 +49,7 @@ class investor(models.Model):
     def __unicode__(self):
         return self.name + ' ' + self.last_name
 
-class invest(models.Model):
+class investment(models.Model):
     investor = models.ForeignKey(investor)
     smes = models.ForeignKey(smes)
     date_invested = models.DateField(auto_now=False)
